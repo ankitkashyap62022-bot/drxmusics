e2250b4507e4b4a32064d401e66f397b9e380741
Gojo infinity <himanshu4hk9@gmail.com>
2026-03-15 09:59:18 +0530

    Update stop.py

--- RessoMusic/plugins/admins/stop.py
+++ RessoMusic/plugins/admins/stop.py
@@ -3,11 +3,11 @@ from pyrogram.types import Message
 
 from RessoMusic import app
 from RessoMusic.core.call import AMBOTOP
-from RessoMusic.utils.database import set_loop
+from RessoMusic.misc import db
+from RessoMusic.utils.database import set_loop, remove_active_chat
 from RessoMusic.utils.decorators import AdminRightsCheck
 from RessoMusic.utils.inline import close_markup
 from config import BANNED_USERS
-
 
 @app.on_message(
     filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
@@ -18,6 +18,18 @@ async def stop_music(cli, message: Message, _, chat_id):
         return
     await AMBOTOP.stop_stream(chat_id)
     await set_loop(chat_id, 0)
+    
+    
+    try:
+        db[chat_id].clear()
+    except:
+        pass
+    try:
+        await remove_active_chat(chat_id)
+    except:
+        pass
+
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
-    )
+        )
+    
